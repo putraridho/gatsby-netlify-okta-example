62852eeab654448965b23ac07b0a5a84b1bb6c48
Ridho <muhammadridhoputra@gmail.com>
2020-06-23 14:40:13 +0700

    so far

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+export default () => (
+  <>
+    Hello World!
+    <p>
+      <Link to="/blog">View Blog</Link>
+      <Link to="/account">My Account</Link>
+    </p>
+  </>
+)
